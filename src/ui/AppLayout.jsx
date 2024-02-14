e3fd5dd6d18ddea87b0a3a545ab9c620c970fb6d
Hojay Lee <hojay@Hojays-MacBook-Air.local>
2024-02-14 17:14:38 +0700

    Sidebar button added

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -5,10 +5,10 @@ import Footer from "./Footer";
 
 function AppLayout() {
   return (
-    <div className="grid grid-cols-12 grid-rows-[auto_auto_1fr_auto] sm:grid-rows-[auto_1fr_auto] w-full h-screen bg-red-300">
+    <div className="grid h-screen w-full grid-cols-12 grid-rows-[auto_auto_1fr_auto] bg-red-300 sm:grid-rows-[auto_1fr_auto]">
       <Header />
       <Sidebar />
-      <main className="bg-green-200 col-span-12 sm:col-span-8 md:col-span-9 lg:col-span-10">
+      <main className="col-span-12 bg-green-200 sm:col-span-8 md:col-span-9 lg:col-span-10">
         <Outlet />
       </main>
       <Footer />
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -2,8 +2,10 @@ import { Link } from "react-router-dom";
 
 function Header() {
   return (
-    <header className="bg-blue-200 p-4 col-span-12 flex items-center border-b-2 border-gray-100 justify-between">
-      <Link to="/">Home</Link>
+    <header className="col-span-12 flex items-center justify-between border-b-2 border-gray-100 bg-blue-200 p-4">
+      <Link to="/" className=" text-blue-500 hover:text-blue-600">
+        Home
+      </Link>
       <p>Hojay</p>
     </header>
   );
