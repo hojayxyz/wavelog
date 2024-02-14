d50c4812fa1acd20248e86073ec2c8f041efc880
Hojay Lee <hojay@Hojays-MacBook-Air.local>
2024-02-14 15:22:09 +0700

    Layout update

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -1,6 +1,6 @@
 function Sidebar() {
   return (
-    <aside className="bg-yellow-200 col-span-2 p-4 border-r-2 border-solid border-gray-100">
+    <aside className="bg-yellow-200 col-span-12 sm:col-span-4 md:col-span-3 lg:col-span-2 p-4 border-r-2 border-solid border-gray-100">
       Sidebar
     </aside>
   );
