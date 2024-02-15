54d4797c1eb52f04dd94761b287c635326d04bc8
Hojay Lee <hojay@Hojays-MacBook-Air.local>
2024-02-15 21:19:13 +0700

    fix env

--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,11 +1,11 @@
 function Home() {
-  console.log(process.env.TESTO);
-  const greetPerson = process.env.TESTO;
+  console.log(process.env.VITE_TESTO);
+  const greetPerson = process.env.VITE_TESTO;
   console.log(`Say hello: ${greetPerson}`);
   return (
     <div className="p-5">
       <h1>Home</h1>
-      <p>{process.env.TESTO}</p>
+      <p>{process.env.VITE_TESTO}</p>
       <p>{greetPerson ? greetPerson : "No Greet Person"}</p>
     </div>
   );
