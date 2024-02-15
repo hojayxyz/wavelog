f7d08d0c1e6174065789842fe32d8cf150313e1d
Hojay Lee <hojay@Hojays-MacBook-Air.local>
2024-02-15 23:30:48 +0700

    Supabase connect

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,17 +1,33 @@
+import { useState } from "react";
 import supabase from "../components/supabase";
 
-async function getPosts() {
-  let { data: postings, error } = await supabase.from("postings").select("*");
-  //   const data = await res.json();
-  console.log(postings);
-  console.log(error);
-}
-
 function Blog() {
+  const [posts, setPosts] = useState([]);
+
+  async function getPosts() {
+    let { data: postings, error } = await supabase.from("postings").select("*");
+    //   const data = await res.json();
+    console.log(postings);
+    console.log(error);
+    setPosts(postings);
+  }
   return (
     <div>
       <div>Blog postings</div>
-      <button onClick={getPosts}>Post</button>
+      <button
+        className="rounded-full bg-yellow-300 px-4 py-2"
+        onClick={getPosts}
+      >
+        Load Posts
+      </button>
+      <div>
+        {posts.map((post) => (
+          <div key={post.id}>
+            <h3 className="font-semibold">{post.post_title}</h3>
+            <p>{post.post_content}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
