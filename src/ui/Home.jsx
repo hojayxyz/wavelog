966db6ba4da09bf2be434fd51461888e485b2a8b
Hojay Lee <hojay@Hojays-MacBook-Air.local>
2024-02-15 21:13:48 +0700

    fix env

--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,4 +1,7 @@
 function Home() {
+  console.log(process.env.TESTO);
+  const greetPerson = process.env.TESTO;
+  console.log(`Say hello: ${greetPerson}`);
   return (
     <div className="p-5">
       <h1>Home</h1>
