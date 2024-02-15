abe6372961894e375a5ef9b8a58690cd98964948
Hojay Lee <hojay@Hojays-MacBook-Air.local>
2024-02-15 21:31:06 +0700

    fix env

--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,11 +1,11 @@
 function Home() {
   console.log(import.meta.env.TESTO);
-  const greetPerson = import.meta.env.VITE_TESTO;
+  const greetPerson = import.meta.env.TESTO;
   console.log(`Say hello: ${greetPerson}`);
   return (
     <div className="p-5">
       <h1>Home</h1>
-      <p>{import.meta.env.VITE_TESTO}</p>
+      <p>{import.meta.env.TESTO}</p>
       <p>{greetPerson ? greetPerson : "No Greet Person"}</p>
     </div>
   );
